6aae6a761614ec7613c3e8c9e6255e03d5ceb1c8
Parth Bhakta <bhaktaparthh@gmail.com>
2022-01-20 09:15:01 -0700

    removing-console.logs and updated logout button

--- auth.js
+++ auth.js
@@ -1,11 +1,9 @@
 const db = require('./db/models');
 
 const loginUser = (req, res, user) => {
-    console.log(`session-auth: ${req.session.auth}`)
     req.session.auth = {
         userId: user.id,
     };
-    console.log(`session-auth-declared: ${req.session.auth}`)
 };
 
 const restoreUser = async (req, res, next) => {
