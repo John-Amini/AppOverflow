377cc6688d69c7ee474a003bf8da1d38ad4c125c
John-Amini <john.amini97@gmail.com>
2022-01-21 17:46:48 -0500

    comment delete try

--- routes/questions.js
+++ routes/questions.js
@@ -6,7 +6,7 @@ const { csrfProtection, asyncHandler } = require('./utils');
 const { check, validationResult } = require('express-validator');
 const { requireAuth } = require('../auth');
 const db = require("../db/models");
-const { Question, Answer } = db;
+const { Question, Answer, Comment } = db;
 
 const questionValidation = [
   check('title')
@@ -26,6 +26,7 @@ router.get('/', requireAuth, function (req, res, next) {
   res.render("questionform", { body: {} });
 });
 
+const commentValidation = [check('new-comment-content').exists({checkFalsy:true}).withMessage("Please provide an actual comment")]
 router.post('/', requireAuth, questionValidation, asyncHandler(async (req, res, next) => {
   const { title, content } = req.body
   const body = req.body;
@@ -54,20 +55,22 @@ router.post('/', requireAuth, questionValidation, asyncHandler(async (req, res, 
 router.get('/:id', asyncHandler(async (req, res, next) => {
   let id = req.params.id;
   const question = await Question.findByPk(id, {
-    include: db.User
+    order: [
+      [db.Answer,'updatedAt', 'DESC']
+    ],
+    include: db.User,
+    include: {
+      model:db.Answer,
+      order:[
+        [db.Comment,'updatedAt','DESC']
+      ],
+      include: {
+        model:db.Comment,
+      }
+     }
   });
-  console.log("askdjhbaskdbaskd")
-  console.log(id)
-  console.log("ALOKINJDSLKASDLKADLKASJDNNLK")
-  const listOfAnswers = await Answer.findAll({
-    raw: true,
-    order: [['updatedAt', 'DESC']],
-    where: {
-      question_id: id
-    }
-  })
-  res.render('question', { question, listOfAnswers });
-
+  const listOfAnswers = question.Answers;
+  res.render('question', { question, listOfAnswers});
 }))
 
 
@@ -83,8 +86,6 @@ router.delete('/:id', requireAuth, asyncHandler(async (req, res, next) => {
     res.errors.push("You cannot delete this");
     res.render("index");
   }
-  console.log("redirectaksjhskjhfbajkfhasdkjfhskjhsflkjhfskljhakjlhfsdkjhf")
-  //redirecting back to main page does not work
   req.method = 'GET'
   console.log(req.method);
   res.redirect(303, '/');
@@ -118,6 +119,7 @@ router.post('/:id/answers', requireAuth, answerValidation, asyncHandler(async (r
   else {
     try {
       const newAnswer = await Answer.create({ question_id: req.params.id, content: newAnswerContent, user_id: res.locals.user.id });
+      await updateUpdatedAt(question);
       res.redirect(`/questions/${req.params.id}`);
     }
     catch (err) {
@@ -155,4 +157,56 @@ router.put('/:id/answers/:answerId',requireAuth,asyncHandler(async(req,res,next)
    }
    res.send("Edit valid");
 }))
+router.post('/:id/comments',requireAuth, commentValidation,asyncHandler(async (req,res,next)=>{
+  let newCommentContent = req.body.newCommentContent;
+  let answerId = req.body.answerId;
+  let questionId = req.params.id;
+  let question = await makeQuery(questionId);
+  let listOfAnswers = question.Answers
+  const validatorErrors = validationResult(req);
+  req.errors =[]
+  const body = req.body;
+  if(newCommentContent === ""){
+    req.errors = validatorErrors.array().map((error) => error.msg);
+    if (req.errors[0] === "Invalid value") req.errors.shift();
+    res.method = 'GET'
+    res.render("question", { body, question, errors: req.errors, listOfAnswers })
+  }
+  else{
+    try {
+      const newComment = await Comment.create({answer_id:answerId, content:newCommentContent, user_id:res.locals.user.id})
+      await updateUpdatedAt(question);
+      return res.redirect(`/questions/${questionId}`)
+    }catch(err){
+      req.errors.push("Something went wrong try again");
+      res.render("question", { body: req.body, errors: req.errors });
+    }
+  }
+  res.redirect(`/questions/${req.params.id}`);
+}))
+
+async function makeQuery(id){
+  const question = await Question.findByPk(id, {
+    order: [
+      [db.Answer,'updatedAt', 'DESC']
+    ],
+    include: db.User,
+    include: {
+      model:db.Answer,
+      order:[
+        [db.Comment,'updatedAt','DESC']
+      ],
+      include: {
+        model:db.Comment,
+      }
+     }
+  });
+  return question;
+}
+async function updateUpdatedAt(question){
+  question.changed('updatedAt',true);
+  await question.update({
+    updatedAt:new Date()
+  })
+}
 module.exports = router;
